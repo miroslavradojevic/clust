06eebe51ae75a3e3f0d95e0fe977046db5949382
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2016-08-07 23:20:17 +0200

    initial commit of the basic clustering methods

---
+++ src/com/braincadet/clust/MeanShiftDemo.java
@@ -0,0 +1,10 @@
+package com.braincadet.clust;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class MeanShiftDemo implements PlugIn {
+    public void run(String s) {
+        IJ.log("...");
+    }
+}
